8d38cb8545eaebdba54f6b97dfa3f70aa4b38bcf
judeakinwale <judeakinwale@gmail.com>
2021-10-27 16:05:09 +0100

    Update company controller and routes

--- Controllers/company_profilie_controller.js
+++ Controllers/company_profilie_controller.js
@@ -1,15 +1,62 @@
 const CompanyProfile = require('../Models/company_profile')
+const errorResponse = require("../Utils/errorResponse")
 
-exports.getCompanyProfile = (req, res) => {
-    const company = CompanyProfile.find()
+// @desc    Get Company Profile
+// @route   GET /api/v1/company
+// @access   Private/All
+exports.getCompanyProfile = async (req, res, next) => {
+    const company = await CompanyProfile.find({})
 
     if (!company) {
         return res.status(404).json({
             success: false,
-            msg: "Company information not found"
+            message: "Company information not found"
+        })
+    } else if (company.length < 1) {
+        return res.status(404).json({
+            success: false,
+            message: "Company information not found"
         })
     }
+    res.status(200).json({
+        success: true,
+        data: company
+    })
+}
 
+exports.createCompanyProfile = async (req, res, next) => {
+    const company = await CompanyProfile.create(req.body)
+
+    if (!company) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid company information"
+        })
+        // return next(new ErrorResponse("An Error Occured, Please Tray Again", 400));
+    }
+    res.status(201).json({
+        success: true,
+        data: company
+    })
+}
+
+exports.updateCompanyProfile = async (req, res, next) => {
+    const company = await CompanyProfile.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+
+    if (!company) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid company information"
+        })
+        // return next(new ErrorResponse("An Error Occured, Please Tray Again", 400));
+    }
     res.status(200).json({
         success: true,
         data: company
--- Routes/company_profile_routes.js
+++ Routes/company_profile_routes.js
@@ -1,8 +1,14 @@
 const Express = require('express')
-const {getCompanyProfile} = require('../Controllers/company_profilie_controller')
+const {
+    getCompanyProfile,
+    createCompanyProfile,
+    updateCompanyProfile
+} = require('../Controllers/company_profilie_controller')
 
 const router = Express.Router()
 
 router.route('/').get(getCompanyProfile)
+router.route('/').post(createCompanyProfile)
+router.route('/:id').put(updateCompanyProfile)
 
 module.exports = router
