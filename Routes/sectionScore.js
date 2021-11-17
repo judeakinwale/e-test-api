160affaf56472ff427adb79643028ea78fbbb314
judeakinwale <judeakinwale@gmail.com>
2021-11-17 18:08:09 +0100

    Update score and test routes

---
+++ Routes/sectionScore.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const advancedResults = require('../Middleware/advancedResults')
+const Score = require('../Models/sectionScore')
+const {
+    protect,
+    authorize,
+    authorizeAdmin
+} = require("../Middleware/auth");
+const {
+    getAllScores,
+    createScore,
+    getScore,
+    updateScore,
+    getAllCandidateScores,
+    getAllTestScores,
+    getAllSectionScores,
+    getCandidateSectionScores,
+    getAllSelfScores,
+    getSelfSectionScores
+} = require('../Controllers/sectionScore')
+
+const router = express.Router()
+
+router.route('/api/v1/section-score/').get(protect, authorizeAdmin, advancedResults(Score), getAllScores)
+router.route('/api/v1/section-score/').post(protect, authorize, createScore)
+router.route('/api/v1/section-score/:id').get(protect, authorize, getScore)
+router.route('/api/v1/section-score/:id').put(protect, authorize, updateScore)
+router.route('/api/v1/section-score/:candidate_id').get(protect, authorizeAdmin, getAllCandidateScores)
+router.route('/api/v1/section-score/:test_id').get(protect, authorizeAdmin, getAllTestScores)
+router.route('/api/v1/section-score/:section_id').get(protect, authorizeAdmin, getAllSectionScores)
+router.route('/api/v1/section-score/:section_id/:candidate_id').get(protect, authorizeAdmin, getCandidateSectionScores)
+router.route('/api/v1/section-score/self').get(protect, authorize, getAllSelfScores)
+router.route('/api/v1/section-score/self/:section_id').get(protect, authorize, getSelfSectionScores)
+
+module.exports = router
--- Routes/test.js
+++ Routes/test.js
@@ -10,14 +10,18 @@ const {
     getAllTests,
     createTest,
     getTest,
-    updateTest
+    updateTest,
+    getTestSections,
+    getAllTestQuestions
 } = require('../Controllers/test')
 
 const router = express.Router()
 
-router.route('/api/v1/test/').get(protect, authorize, advancedResults(Test), getAllTests)
-router.route('/api/v1/test/').post(protect, authorizeAdmin, createTest)
+router.route('/api/v1/test').get(protect, authorize, advancedResults(Test), getAllTests)
+router.route('/api/v1/test').post(protect, authorizeAdmin, createTest)
 router.route('/api/v1/test/:id').get(protect, authorize, getTest)
 router.route('/api/v1/test/:id').put(protect, authorizeAdmin, updateTest)
+router.route('/api/v1/test/:id/sections').get(protect, authorize, getTestSections)
+router.route('/api/v1/test/:id/questions').get(protect, authorize, getAllTestQuestions)
 
 module.exports = router
