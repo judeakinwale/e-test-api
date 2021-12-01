a724fa270625c6ae9d26ded514e8c727c330a047
judeakinwale <judeakinwale@gmail.com>
2021-12-01 10:06:08 +0100

    Add candidate response to docs

--- Routes/candidateResponse.js
+++ Routes/candidateResponse.js
@@ -22,7 +22,7 @@ router.route('/api/v1/candidate-response').post(createCandidateResponse)
 router.route('/api/v1/candidate-response/:id').get(protect, authorize, getCandidateResponse)
 router.route('/api/v1/candidate-response/:id').put(protect, authorize, updateCandidateResponse)
 router.route('/api/v1/candidate-response/:id').delete(protect, authorize, deleteCandidateResponse)
-router.route('/api/v1/candidate-response/self').get(protect, authorize, getCandidateResponseByTestAndQuestion)
+router.route('/api/v1/candidate-response/test/:test_id/question/:question_id').get(protect, authorize, getCandidateResponseByTestAndQuestion)
 // router.route('/api/v1/candidate-response/self').put(protect, authorizeAdmin, updateSelf)
 
 module.exports = router
--- swagger.js
+++ swagger.js
@@ -19,6 +19,7 @@ const endpointsFiles = [
     './Routes/sectionScore',
     './Routes/admin',
     './Routes/candidate',
+    './Routes/candidateResponse',
     './Routes/authentication',
 ];
 // const endpointsFiles = [
